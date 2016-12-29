e265d4873770db7f6c4905244cd9b5e359f2b9c2
enricomartin <enrico.martin@almundo.com>
2016-12-28 21:37:22 -0300

    slides

--- client/components/order-select/orderSelect-controller.js
+++ client/components/order-select/orderSelect-controller.js
@@ -3,13 +3,5 @@
  */
 angular.module('orderSelectController', [])
     .controller('orderSelectCtrl', function($scope){
-        var _self = this;
-        this.orderProp = this.orderBy;
-        console.log(this.orderBy);
-        this.changedValue = function (item){
-            _self.onChangeButton(item);
-            console.log(this.orderBy);
-            console.log(item);
-            this.orderBy = item;
-        }
+
     });
